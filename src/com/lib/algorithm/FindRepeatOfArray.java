91b6dec8fd098c739db7f9a7621e5e332eab0cbe
libing <1736776720@qq.com>
2020-06-25 13:38:11 +0800

    完善查询数组中重复的数及重复次数

--- src/com/lib/algorithm/FindRepeatOfArray.java
+++ src/com/lib/algorithm/FindRepeatOfArray.java
@@ -8,7 +8,7 @@ import com.lib.utils.OutUtil;
 import com.lib.utils.SortUtil;
 
 /**
- * 查询除数组中重复的数
+ * 查询除数组中重复的数及重复次数
  * 
  * 解题思路：将该数组的值放置于数组值的数组中的位置。
  * example：  
@@ -22,7 +22,7 @@ public class FindRepeatOfArray  {
 
 	
 	public static void main(String[] args) {
-		int[] nums = {2, 3, 1, 0, 2, 5, 3};
+		int[] nums = {2, 3, 1, 0, 2, 5, 3,3,3,2,5,0};
 		Map<Integer,Integer> numberMap = findRepeat(nums);
 		OutUtil.outMap(numberMap);
 	}
@@ -31,15 +31,14 @@ public class FindRepeatOfArray  {
 	public static Map<Integer,Integer>  findRepeat(int[] nums){
 		Map<Integer,Integer> arrMap = new HashMap<>();
 		for (int i = 0	; i < nums.length; ++i) {
-			int count = 0;
 			while (i!=nums[i]) {
 				if(nums[i] == nums[nums[i]]){
-					count++;
-					continue;
+					//存在Map集合中，取出集合的value 值加1，否则置1
+					arrMap.put(nums[i],arrMap.get(nums[i])!=null?(arrMap.get(nums[i])+1):1);
+					break;
 				}
 				SortUtil.swap(nums, i, nums[i]);
 			}
-			arrMap.put(nums[i], count);
 		}
 		return arrMap;
 	}
